1a137026919b7f29696220743b2d94cd120a3c14
hun <fidabspd@gmail.com>
2023-09-14 17:47:13 +0900

    feat: Implement Encoder and Decoder

--- tutorial_and_demo/CNF/network.py
+++ tutorial_and_demo/CNF/network.py
@@ -5,28 +5,87 @@ from torch import nn
 class ImageEncoder(nn.Module):
     def __init__(
         self,
-        in_channels=1,
-        hidden_channels=32,
-        latent_channels=16,
-        conv_kernel_size=3,
-        conv_stride=1,
-        pool_kernel_size=3,
-        pool_stride=2,
-        dropout_ratio=0.1,
-    ):
+        image_size: int = 28,
+        in_channels: int = 1,
+        hidden_channels: int = 32,
+        latent_channels: int = 16,
+        kernel_size: int = 3,
+        dropout_ratio: float = 0.1,
+    ) -> None:
         super().__init__()
-        self.conv0 = nn.Conv2d(in_channels, hidden_channels, conv_kernel_size, conv_stride)
-        self.conv1 = nn.Conv2d(hidden_channels, hidden_channels, conv_kernel_size, conv_stride)
-        self.conv2 = nn.Conv2d(hidden_channels, latent_channels, conv_kernel_size, conv_stride)
-        self.pool = nn.AvgPool2d(pool_kernel_size, pool_stride, padding=0)
+        self.conv0 = nn.Conv2d(in_channels, hidden_channels, kernel_size, 1)
+        self.conv1 = nn.Conv2d(hidden_channels, hidden_channels, kernel_size, 1)
+        self.conv2 = nn.Conv2d(hidden_channels, hidden_channels, kernel_size, 1)
+        self.flatten = nn.Flatten(start_dim=1)
+        self.linear = nn.Linear((image_size - (kernel_size - 1) * 3) ** 2 * hidden_channels, latent_channels)
         self.dropout = nn.Dropout(dropout_ratio)
 
-    def forward(self, input):
-        batch_size = input.size(0)
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
         output = self.dropout(torch.tanh(self.conv0(input)))
-        output = self.pool(output)
         output = self.dropout(torch.tanh(self.conv1(output)))
-        output = self.pool(output)
-        output = self.conv2(output)
-        output = output.view(batch_size, -1)
+        output = self.dropout(torch.tanh(self.conv2(output)))
+        output = self.flatten(output)
+        output = self.linear(output)
+        return output
+
+
+class ImageDecoder(nn.Module):
+    def __init__(
+        self,
+        image_size: int = 28,
+        latent_channels: int = 16,
+        hidden_channels: int = 32,
+        out_channels: int = 1,
+        kernel_size: int = 3,
+        dropout_ratio: float = 0.1,
+    ) -> None:
+        super().__init__()
+        self.hidden_channels = hidden_channels
+        self.latent_image_size = image_size - 3 * (kernel_size - 1)
+        self.linear = nn.Linear(latent_channels, (image_size - (kernel_size - 1) * 3) ** 2 * hidden_channels)
+        self.convt0 = nn.ConvTranspose2d(hidden_channels, hidden_channels, kernel_size, 1)
+        self.convt1 = nn.ConvTranspose2d(hidden_channels, hidden_channels, kernel_size, 1)
+        self.convt2 = nn.ConvTranspose2d(hidden_channels, out_channels, kernel_size, 1)
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        output = self.dropout(torch.tanh(self.linear(input)))
+        output = output.view(-1, self.hidden_channels, self.latent_image_size, self.latent_image_size)
+        output = self.dropout(torch.tanh(self.convt0(output)))
+        output = self.dropout(torch.tanh(self.convt1(output)))
+        output = self.convt2(output)
+        return output
+
+
+class AECNF(nn.Module):
+    def __init__(
+        self,
+        image_size: int = 28,
+        in_channels: int = 1,
+        hidden_channels: int = 32,
+        latent_channels: int = 16,
+        kernel_size: int = 3,
+        dropout_ratio: float = 0.1,
+    ) -> None:
+        super().__init__()
+        self.image_encoder = ImageEncoder(
+            image_size=image_size,
+            in_channels=in_channels,
+            hidden_channels=hidden_channels,
+            latent_channels=latent_channels,
+            kernel_size=kernel_size,
+            dropout_ratio=dropout_ratio,
+        )
+        self.image_decoder = ImageDecoder(
+            image_size=image_size,
+            latent_channels=latent_channels,
+            hidden_channels=hidden_channels,
+            out_channels=in_channels,
+            kernel_size=kernel_size,
+            dropout_ratio=dropout_ratio,
+        )
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        latent = self.image_encoder(input)
+        output = self.image_decoder(latent)
         return output
