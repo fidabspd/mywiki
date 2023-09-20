b2d432c414cde82755e81606e6a3d78541248ff1
hun <fidabspd@gmail.com>
2023-09-20 10:49:09 +0900

    feat: Implement GAN loss

--- tutorial_and_demo/CNF/gen_mnist_tutorial.py
+++ tutorial_and_demo/CNF/gen_mnist_tutorial.py
@@ -41,51 +41,95 @@ def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-def discriminator_loss(
-    real_output: torch.Tensor,
-    gen_output: torch.Tensor,
-):
-    real_loss = torch.log(real_output)
-    real_loss = real_loss.flatten(start_dim=1).sum(dim=1).mean()
-    gen_loss = torch.log(1 - gen_output)
-    gen_loss = gen_loss.flatten(start_dim=1).sum(dim=1).mean()
-    loss = -(real_loss + gen_loss)
-    return loss, real_loss, gen_loss
+def calculate_gan_discriminator_loss(
+    disc_true_output: torch.Tensor,
+    disc_pred_output: torch.Tensor,
+) -> torch.Tensor:
+    disc_real_true_loss = torch.log(disc_true_output)
+    disc_real_true_loss = disc_real_true_loss.flatten(start_dim=1).sum(dim=1).mean()
+    disc_pred_true_loss = torch.log(1 - disc_pred_output)
+    disc_pred_true_loss = disc_pred_true_loss.flatten(start_dim=1).sum(dim=1).mean()
+    gan_discriminator_loss = -(disc_real_true_loss + disc_pred_true_loss)
+    return gan_discriminator_loss, disc_real_true_loss, disc_pred_true_loss
 
 
-def generator_loss(gen_output: torch.Tensor):
-    gen_loss = torch.log(gen_output)
-    gen_loss = gen_loss.flatten(start_dim=1).sum(dim=1).mean()
-    loss = -gen_loss
-    return loss, gen_loss
+def calculate_gan_generator_loss(disc_pred_output: torch.Tensor) -> torch.Tensor:
+    disc_fake_pred_loss = torch.log(disc_pred_output)
+    disc_fake_pred_loss = disc_fake_pred_loss.flatten(start_dim=1).sum(dim=1).mean()
+    gan_generator_loss = -disc_fake_pred_loss
+    return gan_generator_loss, disc_fake_pred_loss
 
 
-def feature_map_loss(real_feature_maps: torch.Tensor, gen_feature_maps: torch.Tensor) -> torch.Tensor:
-    loss = 0
-    for real_feature_map, gen_feature_map in zip(real_feature_maps, gen_feature_maps):
-        real_feature_map = real_feature_map.flatten(start_dim=1)
-        gen_feature_map = gen_feature_map.flatten(start_dim=1)
-        loss += torch.abs(real_feature_map - gen_feature_map).sum(dim=1).mean()
-    return loss
+def calculate_gan_feature_map_loss(disc_true_feature_maps: torch.Tensor, disc_pred_feature_maps: torch.Tensor) -> torch.Tensor:
+    gan_feature_map_loss = 0
+    for disc_true_feature_map, disc_pred_feature_map in zip(disc_true_feature_maps, disc_pred_feature_maps):
+        disc_true_feature_map = disc_true_feature_map.flatten(start_dim=1)
+        disc_pred_feature_map = disc_pred_feature_map.flatten(start_dim=1)
+        gan_feature_map_loss += torch.abs(disc_true_feature_map - disc_pred_feature_map).sum(dim=1).mean()
+    return gan_feature_map_loss
 
 
-def calculate_loss(
+def calculate_vae_loss(
     image_true: torch.Tensor,
     imgae_pred: torch.Tensor,
-    x_probs: torch.Tensor,
     mean: torch.Tensor,
     std: torch.Tensor,
-    cnf_loss_weight: float = 3.0,
-):
+) -> torch.Tensor:
     recon_loss = image_true * torch.log(imgae_pred) + (1 - image_true) * torch.log(1 - imgae_pred)
     recon_loss = torch.flatten(recon_loss, start_dim=1).sum(dim=1).mean()
     kl_divergence = torch.square(mean) + torch.square(std) - torch.log(torch.square(std)) - 1
     kl_divergence = 0.5 * torch.flatten(kl_divergence, start_dim=1).sum(dim=1).mean()
     elbo = recon_loss - kl_divergence
-    cnf_loss = -x_probs
-    vae_loss = -elbo
-    total_loss = vae_loss + cnf_loss_weight * cnf_loss
-    return total_loss, recon_loss, kl_divergence, cnf_loss
+    loss = -elbo
+    return loss, recon_loss, kl_divergence
+
+
+def calculate_cnf_loss(x_probs: torch.Tensor) -> torch.Tensor:
+    loss = -x_probs
+    return loss, x_probs
+
+
+def calculate_final_discriminator_loss(
+    disc_true_output: torch.Tensor,
+    disc_pred_output: torch.Tensor,
+    return_only_final_loss: bool = True,
+) -> torch.Tensor:
+    final_discriminator_loss, disc_real_true_loss, disc_real_pred_loss = calculate_gan_discriminator_loss(disc_true_output, disc_pred_output)
+    if return_only_final_loss:
+        return final_discriminator_loss
+    else:
+        return final_discriminator_loss, disc_real_true_loss, disc_real_pred_loss
+
+
+def calculate_final_generator_loss(
+    image_true: torch.Tensor,
+    imgae_pred: torch.Tensor,
+    disc_pred_output: torch.Tensor,
+    disc_true_feature_maps: torch.Tensor,
+    disc_pred_feature_maps: torch.Tensor,
+    mean: torch.Tensor,
+    std: torch.Tensor,
+    cnf_probs: torch.Tensor,
+    gan_feature_map_loss_weigth: float = 1.0,
+    gan_generator_loss_weight: float = 1.0,
+    vae_loss_weight: float = 1.0,
+    cnf_loss_weight: float = 1.0,
+    return_only_final_loss: bool = True,
+):
+    gan_feature_map_loss = calculate_gan_feature_map_loss(disc_true_feature_maps, disc_pred_feature_maps)
+    gan_generator_loss, disc_fake_pred_loss = calculate_gan_generator_loss(disc_pred_output)
+    vae_loss, recon_loss, kl_divergence = calculate_vae_loss(image_true, imgae_pred, mean, std)
+    cnf_loss, cnf_probs = calculate_cnf_loss(cnf_probs)
+    final_generator_loss = (
+        gan_feature_map_loss_weigth * gan_feature_map_loss
+        + gan_generator_loss_weight * gan_generator_loss
+        + vae_loss_weight * vae_loss
+        + cnf_loss_weight * cnf_loss
+    )
+    if return_only_final_loss:
+        return final_generator_loss
+    else:
+        return final_generator_loss, gan_feature_map_loss, disc_fake_pred_loss, recon_loss, kl_divergence, cnf_probs
 
 
 def visualize_inference_result(
